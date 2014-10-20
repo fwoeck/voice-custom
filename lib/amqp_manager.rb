629c64f14cd356dc772f9759ee0c72cfb1c6ea77
Frank Wöckener <fwoeck@gmail.com>
2014-10-20 04:15:55 +0000

    use celluloid worker pool for AmqpManager

--- lib/amqp_manager.rb
+++ lib/amqp_manager.rb
@@ -1,58 +1,89 @@
-module AmqpManager
+class AmqpManager
+  include Celluloid
+
+  TOPICS = [:rails, :custom]
+
+
+  TOPICS.each { |name|
+    class_eval %Q"
+      def #{name}_channel
+        @#{name}_channel ||= connection.create_channel
+      end
+    "
+
+    class_eval %Q"
+      def #{name}_xchange
+        @#{name}_xchange ||= #{name}_channel.topic('voice.#{name}', auto_delete: false)
+      end
+    "
+
+    class_eval %Q"
+      def #{name}_queue
+        @#{name}_queue ||= #{name}_channel.queue('voice.#{name}', auto_delete: false)
+      end
+    "
+  }
+
+
+  def rails_publish(payload)
+    data = Marshal.dump(payload)
+    rails_xchange.publish(data, routing_key: 'voice.rails')
+  end
+
+
+  def connection
+    establish_connection unless @@connection
+    @@connection
+  end
+
+
+  def shutdown
+    connection.close
+  end
+
+
+  def establish_connection
+    @@connection = Bunny.new(
+      host:     Custom.conf['rabbit_host'],
+      user:     Custom.conf['rabbit_user'],
+      password: Custom.conf['rabbit_pass']
+    ).tap { |c| c.start }
+  rescue Bunny::TCPConnectionFailed
+    sleep 1
+    retry
+  end
+
+
+  def start
+    establish_connection
+
+    custom_queue.bind(custom_xchange, routing_key: 'voice.custom')
+    custom_queue.subscribe { |delivery_info, metadata, payload|
+      Marshal.load(payload).handle_message
+    } if ENV['SUBSCRIBE_AMQP']
+  end
+
+
   class << self
 
+    def start
+      # TODO This will suppress warnings at exit, but could also
+      #       mask potential problems. Try to remove after a while:
+      #
+      Celluloid.logger = nil
 
-    def custom_channel
-      Thread.current[:custom_channel] ||= @connection.create_channel
-    end
-
-    def custom_xchange
-      Thread.current[:custom_xchange] ||= custom_channel.topic('voice.custom', auto_delete: false)
-    end
-
-    def custom_queue
-      Thread.current[:custom_queue] ||= custom_channel.queue('voice.custom', auto_delete: false)
-    end
-
-
-    def rails_channel
-      Thread.current[:rails_channel] ||= @connection.create_channel
-    end
-
-    def rails_xchange
-      Thread.current[:rails_xchange] ||= rails_channel.topic('voice.rails', auto_delete: false)
-    end
-
-    def rails_publish(payload)
-      data = Marshal.dump(payload)
-      rails_xchange.publish(data, routing_key: 'voice.rails')
+      Celluloid::Actor[:amqp] = AmqpManager.pool
+      @@manager ||= new.tap { |m| m.start }
     end
 
 
     def shutdown
-      @connection.close
+      @@manager.shutdown
     end
 
 
-    def establish_connection
-      @connection = Bunny.new(
-        host:     Custom.conf['rabbit_host'],
-        user:     Custom.conf['rabbit_user'],
-        password: Custom.conf['rabbit_pass']
-      ).tap { |c| c.start }
-    rescue Bunny::TCPConnectionFailed
-      sleep 1
-      retry
-    end
-
-
-    def start
-      establish_connection
-
-      custom_queue.bind(custom_xchange, routing_key: 'voice.custom')
-      custom_queue.subscribe { |delivery_info, metadata, payload|
-        Marshal.load(payload).handle_message
-      } if ENV['SUBSCRIBE_AMQP']
+    def rails_publish(*args)
+      Celluloid::Actor[:amqp].async.rails_publish(*args)
     end
   end
 end
