3aa78071692293e097520bdd70d5ba8ad7297935
Frank Wöckener <fwoeck@gmail.com>
2014-10-31 09:33:20 +0000

    refactor CustomerSearch class

--- lib/customer_search.rb
+++ lib/customer_search.rb
@@ -1,6 +1,6 @@
 class CustomerSearch
 
-  attr_accessor :c_opts, :c_query, :h_opts, :h_query, :customer_ids, :history
+  attr_accessor :opts, :c_opts, :h_opts, :customer_ids, :history
 
 
   def self.search(opts)
@@ -14,31 +14,39 @@ class CustomerSearch
   end
 
 
-  def initialize(opts)
-    size = sanitized_size(opts)
-    time = sanitized_timespan(opts)
+  def initialize(_opts)
+    @opts = _opts
+    size  = sanitized_size
+    time  = sanitized_timespan
 
-    @c_opts  = build_query_for(opts, :c, size)
-    @h_opts  = build_query_for(opts, :h, size, time)
-
-    @c_query = !opts[:c].blank?
-    @h_query = !opts[:h].blank?
+    @c_opts = build_query_for(:c, size)
+    @h_opts = build_query_for(:h, size, time)
   end
 
 
-  def sanitized_timespan(opts)
+  def c_query?
+    !opts[:c].blank?
+  end
+
+
+  def h_query?
+    !opts[:h].blank?
+  end
+
+
+  def sanitized_timespan
     delta = opts.fetch(:t, '4w')
     delta[/\A[0-9]+[mhdwM]\z/] ? "now-#{delta}" : 'now-4w'
   end
 
 
-  def sanitized_size(opts)
+  def sanitized_size
     size = opts.fetch(:s, 100)
     size > 100 ? 100 : size
   end
 
 
-  def build_query_for(opts, key, size, from=nil)
+  def build_query_for(key, size, from=nil)
     {
       body: {
         query: {
@@ -67,7 +75,7 @@ class CustomerSearch
         terms: {
           customer_id: customer_ids.map(&:to_s)
         }
-      } if c_query
+      } if c_query?
     }
   end
 
@@ -80,7 +88,7 @@ class CustomerSearch
 
 
   def find_customer_ids
-    return [] unless c_query
+    return [] unless c_query?
     Customer.es.search(c_opts).results.map(&:id)
   rescue => e
     puts "#{Time.now.utc} :: An error happened: #{e.message}"
@@ -89,10 +97,10 @@ class CustomerSearch
 
 
   def find_history_entries
-    return {} unless h_query
-    opts = scoped_history_query
+    return {} unless h_query?
+    _opts = scoped_history_query
 
-    HistoryEntry.es.search(opts).results
+    HistoryEntry.es.search(_opts).results
                 .group_by { |he| he.customer_id }
   rescue => e
     puts "#{Time.now.utc} :: An error happened: #{e.message}"
@@ -102,15 +110,15 @@ class CustomerSearch
 
   def aggregate_customers
     filtered_customers.each_with_object([]) { |cust, result|
-      assemble_entries_from(history, cust) if h_query
+      assemble_entries_from(history, cust) if h_query?
       result << cust
     }
   end
 
 
   def filtered_customers
-    keys = c_query ? customer_ids : (h_query ? history.keys : [])
-    keys = h_query ? keys.map(&:to_s) & history.keys : keys
+    keys = c_query? ? customer_ids : (h_query? ? history.keys : [])
+    keys = h_query? ? keys.map(&:to_s) & history.keys : keys
     Customer.where(:_id.in => keys).without('history_entries')
   end
 
